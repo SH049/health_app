5b54751f55f128895ac47d5af455d0c1bf645413
SH049 <146598693+SH049@users.noreply.github.com>
2024-04-27 10:49:22 +0900

    日付の重複禁止
    日付の変更は可能だけど、validationで同じ日付の日記を作れないようにした。

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,6 @@
 class Diary < ApplicationRecord
   belongs_to :user
   validates :text, presence:true
+  validates :start_time, uniqueness: { scope: :user_id,
+                                       message: "同じ日付の日記は一つしか作れません" }
 end
