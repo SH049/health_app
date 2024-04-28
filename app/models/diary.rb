78452ff49d11f1bfbc374a32cd4af7970571ca54
SH049 <146598693+SH049@users.noreply.github.com>
2024-04-28 10:14:46 +0900

    add_indexでdiariesにuniqueを追加
    user_idにつき、start_timeは一つずつしか持てないことを明示するために追加。

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,6 +1,8 @@
 class Diary < ApplicationRecord
   belongs_to :user
-  validates :text, presence:true
-  validates :start_time, uniqueness: { scope: :user_id,
-                                       message: "同じ日付の日記は一つしか作れません" }
+  validates :text, presence: true
+  validates :start_time, uniqueness: {
+    scope: :user_id,
+    message: "同じ日付の日記は一つしか作れません",
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_27_020931) do
+ActiveRecord::Schema.define(version: 2024_04_28_010606) do
 
   create_table "diaries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2024_04_27_020931) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "feeling"
+    t.index ["start_time", "user_id"], name: "index_diaries_on_start_time_and_user_id", unique: true
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
